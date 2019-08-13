b719164f8683d46589e8f9ef031d479d9ee038dc
Livingstone Asabahebwa <ahebwa49@gmail.com>
2019-08-13 16:42:21 +0300

    Initial commit

---
+++ src/Testimonials.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Testimonials = () => {
+  console.log(this.props);
+  return (
+    <div>
+      <h1>Testimonialst</h1>
+    </div>
+  );
+};
+
+export default Testimonials;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  Route,
+  NavLink,
+  BrowserRouter as Router,
+  Switch
+} from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import About from "./About";
+import Services from "./Services";
+import Testimonials from "./Testimonials";
+import Contact from "./Contact";
+import Activities from "./Activities";
+import NotFound from "./NotFound";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from "./serviceWorker";
+
+const routing = (
+  <Router>
+    <ul>
+      <li>
+        <NavLink exact activeClassName="active" to="/">
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName="active" to="/about">
+          About
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName="active" to="/services">
+          Services
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName="active" to="/activities">
+          Activities
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName="active" to="/testimonials">
+          Testimonials
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName="active" to="/contact">
+          Contact
+        </NavLink>
+      </li>
+    </ul>
+    <div>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/about" component={About} />
+        <Route path="/services" component={Services} />
+        <Route path="/activities" component={Activities} />
+        <Route path="/testimonials" component={Testimonials} />
+        <Route path="/contact" component={Contact} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
