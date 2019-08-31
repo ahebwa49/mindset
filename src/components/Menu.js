386e005613cc70e136acb9e6557d91c782c5809e
Livingstone Asabahebwa <ahebwa49@gmail.com>
2019-08-31 15:43:49 +0300

    add nav menu for tablet layout

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -37,7 +37,89 @@ class NavMenu extends Component {
             </Container>
           </Menu>
         </Responsive>
-        <Responsive {...Responsive.onlyTablet}>Tablet</Responsive>
+        <Responsive {...Responsive.onlyTablet}>
+          <Menu>
+            <Container>
+              <Menu.Item as="a" header>
+                <Image size="small" src="/static/mindset.png" />
+              </Menu.Item>
+
+              <Menu.Menu position="right">
+                <Menu.Item
+                  as={Link}
+                  to="/"
+                  name="home"
+                  active={activeItem === "home"}
+                  onClick={this.handleItemClick}
+                >
+                  Home
+                </Menu.Item>
+
+                <Menu.Item
+                  as={Link}
+                  to="/about"
+                  name="about"
+                  active={activeItem === "about"}
+                  onClick={this.handleItemClick}
+                >
+                  About
+                </Menu.Item>
+
+                <Menu.Item
+                  as={Link}
+                  to="/services"
+                  name="services"
+                  active={activeItem === "services"}
+                  onClick={this.handleItemClick}
+                >
+                  Services
+                </Menu.Item>
+
+                <Menu.Item
+                  as={Link}
+                  to="/contact"
+                  name="contact"
+                  active={activeItem === "contact"}
+                  onClick={this.handleItemClick}
+                >
+                  Contact
+                </Menu.Item>
+
+                <Menu.Item>
+                  {this.props.username ? (
+                    <UserMenu username={this.props.username} />
+                  ) : (
+                    <Button.Group>
+                      <Button
+                        name="sign in"
+                        active={activeItem === "sign in"}
+                        onClick={this.handleItemClick}
+                        as={Link}
+                        to="/login"
+                      >
+                        Sign In
+                      </Button>
+
+                      <Button.Or />
+
+                      <Button
+                        name="register"
+                        active={activeItem === "register"}
+                        onClick={this.handleItemClick}
+                        as={Link}
+                        to="/register"
+                        primary={true}
+                        style={{ marginLeft: "0.5em" }}
+                      >
+                        Register
+                      </Button>
+                    </Button.Group>
+                  )}
+                </Menu.Item>
+              </Menu.Menu>
+            </Container>
+          </Menu>
+        </Responsive>
 
         <Responsive {...Responsive.onlyComputer}>
           <Menu>
